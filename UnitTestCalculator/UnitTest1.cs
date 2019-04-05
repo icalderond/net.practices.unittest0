3a63fb0f61d57af7b74309bf1f3fa9242fa74197
Israel Calderon <icalderond@outlook.com>
2019-04-05 16:22:59 -0600

    Add project files.

---
+++ UnitTestCalculator/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
